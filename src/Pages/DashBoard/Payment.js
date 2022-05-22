ca7e82337fc8b20ea7bba7a1a25f56ea69d8cba7
marjan <74914575+Marjan-Binta-Aziz@users.noreply.github.com>
2022-05-23 00:15:02 +0600

    ShowdatatoPaymentroute

--- src/Pages/DashBoard/Payment.js
+++ src/Pages/DashBoard/Payment.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../../Shared/Loading';
 
 const Payment = () => {
     const {id} = useParams();
+    const url = `http://localhost:5000/booking/${id}`;
 
+    const { data: myAppointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
-        <div>
-            <h1 className='text-2xl text-orange-600 font-semibold uppercase p-3'>Payment for: {id}</h1>
+        <div className='w-full'>
+            <div className="card bg-base-100 shadow-xl my-12">
+                <div className="card-body">
+                    <p className="text-success font-bold">Hello, {myAppointment.name}</p>
+                    <h2 className="card-title">Please Pay for {myAppointment.treatment}</h2>
+                    <p>Your Appointment: <span className='text-orange-700'>{myAppointment.date}</span> at {myAppointment.slot}</p>
+                    <p>Please pay: ${myAppointment.price}</p>
+                </div>
+            </div>
+            {/* <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm myAppointment={myAppointment} />
+                    </Elements>
+                </div>
+            </div> */}
         </div>
     );
 };
